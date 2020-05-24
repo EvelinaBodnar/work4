7d40b55984c75571460da8ce9c2a82b68d6faf35
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-24 23:33:56 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,26 @@
+INSERT INTO Company (company) 
+VALUES ('Soma');
+
+INSERT INTO Company (company) 
+VALUES ('Bonnat');
+
+INSERT INTO Company (company) 
+VALUES ('A.Morin');
+
+INSERT INTO Bean (bean_type) 
+VALUES ('Trinitario');
+
+INSERT INTO Bean (bean_type) 
+VALUES ('Criollo');
+
+INSERT INTO Bean(bean_type) 
+VALUES ('Forastero');
+
+INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc) 
+VALUES ('1','Madagascar', 'Soma', 'Trinitario', 70);	
+
+INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc) 
+VALUES ('7','Quilla ', 'Soma', 'Criollo', 72);
+
+INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc) 
+VALUES ('9','Akata ', 'A.Morin', 'Forastero', 65);
